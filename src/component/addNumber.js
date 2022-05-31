12f51b45561e3cab7ac64ecaaec1bf6a9fae2e90
abhishek <abhishekparmarjnv@gmail.com>
2022-05-31 22:06:12 +0530

    ui change

--- src/component/addNumber.js
+++ src/component/addNumber.js
@@ -6,7 +6,7 @@ function AddNumber(props) {
 
     return (
         <div className='is-flex is-justify-content-space-between'>
-            <p className='has-text-white is-size-4 has-text-weight-bold'>{counter}</p>
+            <p className='has-text-white is-size-4 has-text-weight-bold'>{`Redux value : ${counter}`}</p>
             <button className='button is-outlined is-white is-rounded' onClick={props.apiCall}>Refresh</button>
         </div>
     )
